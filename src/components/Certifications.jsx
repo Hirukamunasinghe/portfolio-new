32edf076af8df8d56204a5ec6cb18ee6fd355aee
Hirukamunasinghe <munasinghehiruka@gmail.com>
2024-06-03 17:58:48 +0530

    certifications-update

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import Python from "./python-cert.jpg";
 import Hackathon from "./hackathon.png";
+import webDev from "./webdev.jpg";
 
 const URLH= "https://www.linkedin.com/in/hiruka-munasinghe-b82752217/overlay/1714037563826/single-media-viewer/?profileId=ACoAADa_7RIBZPDbFDStLOBbwcmgheXdvJ3JICo"
 const URLP = "https://www.udemy.com/certificate/UC-2a2c8b36-2de1-4797-bc03-79efb4850772/"
+const URLW = "https://www.linkedin.com/in/hiruka-munasinghe-b82752217/details/certifications/"
 
 const Certifications = () => {
     const [animated, setAnimated] = useState(false);
@@ -32,6 +34,7 @@ const Certifications = () => {
             <div className="images-div">
                 <a href={URLP}><img className="cert-img" src={Python} alt=""/></a>
                 <a href={URLH}><img className="cert-img" src={Hackathon} alt=""/></a>
+                <a href={URLW}><img className="cert-img" src={webDev} alt=""/></a>
             </div>
         </div>
     );
