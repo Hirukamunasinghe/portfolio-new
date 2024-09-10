b144cc5d704b8520186b4c1f1df7d3655d2c9484
Hirukamunasinghe <munasinghehiruka@gmail.com>
2024-09-10 19:51:08 +0530

    commit

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -7,6 +7,7 @@ import uiux from "./ui-ux-cert.jpg";
 const URLH= "https://www.linkedin.com/in/hiruka-munasinghe-b82752217/overlay/1714037563826/single-media-viewer/?profileId=ACoAADa_7RIBZPDbFDStLOBbwcmgheXdvJ3JICo"
 const URLP = "https://www.udemy.com/certificate/UC-2a2c8b36-2de1-4797-bc03-79efb4850772/"
 const URLW = "https://www.linkedin.com/in/hiruka-munasinghe-b82752217/details/certifications/"
+const URLUI = "https://www.linkedin.com/in/hiruka-munasinghe-b82752217/overlay/1725959063703/single-media-viewer/?profileId=ACoAADa_7RIBZPDbFDStLOBbwcmgheXdvJ3JICo"
 
 const Certifications = () => {
     const [animated, setAnimated] = useState(false);
@@ -36,7 +37,7 @@ const Certifications = () => {
                 <a href={URLP}><img className="cert-img" src={Python} alt=""/></a>
                 <a href={URLH}><img className="cert-img" src={Hackathon} alt=""/></a>
                 <a href={URLW}><img className="cert-img" src={webDev} alt=""/></a>
-                <a href={URLW}><img className="cert-img" src={uiux} alt=""/></a>
+                <a href={URLUI}><img className="cert-img" src={uiux} alt=""/></a>
             </div>
         </div>
     );
