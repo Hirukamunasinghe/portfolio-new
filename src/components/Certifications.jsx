8f5874454113c716268d11cca931a296872c4162
Hirukamunasinghe <munasinghehiruka@gmail.com>
2024-09-10 14:49:34 +0530

    commit

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Python from "./python-cert.jpg";
 import Hackathon from "./hackathon.png";
 import webDev from "./webdev.jpg";
+import uiux from "./ui-ux-cert.jpg";
 
 const URLH= "https://www.linkedin.com/in/hiruka-munasinghe-b82752217/overlay/1714037563826/single-media-viewer/?profileId=ACoAADa_7RIBZPDbFDStLOBbwcmgheXdvJ3JICo"
 const URLP = "https://www.udemy.com/certificate/UC-2a2c8b36-2de1-4797-bc03-79efb4850772/"
@@ -35,6 +36,7 @@ const Certifications = () => {
                 <a href={URLP}><img className="cert-img" src={Python} alt=""/></a>
                 <a href={URLH}><img className="cert-img" src={Hackathon} alt=""/></a>
                 <a href={URLW}><img className="cert-img" src={webDev} alt=""/></a>
+                <a href={URLW}><img className="cert-img" src={uiux} alt=""/></a>
             </div>
         </div>
     );
